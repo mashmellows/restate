56b9db6ef5026cacb43247974feb4de6a92f377b
Mash Wimalawardana <mashdroidd@gmail.com>
2018-06-18 16:02:19 +1000

    Added Markers on the Maps along with Popups
    Currently using the built in Popups but will convert to a  system thats a bit better.

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { BarLoader } from 'react-spinners';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Popup, Marker } from "react-mapbox-gl";
 import { ZoomControl } from "react-mapbox-gl";
 import Rodal from 'rodal';
+import { Tooltip } from 'react-bootstrap';
 
 import './app.css';
 import 'rodal/lib/rodal.css';
@@ -14,6 +15,8 @@ const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoieHVyYSIsImEiOiJjamlpam5mczQxdGZjM3F0NDU4d2Z5NWJ2In0.bStw02eNHWj4PhkmzWDeOg"
 });
 
+
+
 class LandingPage extends Component {
 
   constructor(props) {
@@ -21,10 +24,15 @@ class LandingPage extends Component {
     this.state = {
       data: null,
       center: [144.963169, -37.814251],
-      visible: false
+      visible: false,
+      distance: null,
+      popup: false,
     }
+    // state binding
     this.showPosition = this.showPosition.bind(this);
     this.show = this.show.bind(this);
+    this.haversine = this.haversine.bind(this);
+
   }
 
   show(data) {
@@ -40,6 +48,36 @@ class LandingPage extends Component {
     return this.props.data.homes
   }
 
+  haversine(targetCoordinates, data) {
+
+    // haversine formula ACOS(SIN(Lat1)*SIN(Lat2) +COS(Lat1)*COS(Lat2)*COS(Lon2-Lon1)) *6371
+    // this does not account for roads. Just straightline distance.
+    // Way around it would be to put lat/long points on the road path and then sum up the total distance.
+
+    let earthRadius = 6371;
+
+    let userCoordinates = this.state.center;
+
+    let userLat = userCoordinates[1]
+    let userLong = userCoordinates[0]
+
+    let targetLat = targetCoordinates[1];
+    let targetLong = targetCoordinates[0];
+
+    let deltaLat = (targetLat-userLat) * (Math.PI / 180)
+    let deltaLong = (targetLong-userLong) * (Math.PI / 180)
+
+    let value = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) + Math.cos(userLat) * Math.cos(targetLat) * Math.sin(deltaLong / 2) * Math.sin(deltaLong / 2)
+    let calculate = 2 * Math.atan2(Math.sqrt(value), Math.sqrt(1-value))
+
+    let haversine = earthRadius * calculate
+
+    this.setState({ distance: haversine })
+
+    this.show(data);
+  }
+
+
   getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(this.showPosition);
@@ -48,13 +86,19 @@ class LandingPage extends Component {
         alert('Geolocation is not supported on this browser')
       }
   }
+
   showPosition(position) {
     let coordinates = [position.coords.longitude, position.coords.latitude]
+
     this.setState({ center: coordinates})
   }
 
   componentDidMount() {
     this.getLocation()
+  }
+
+  showTooltip() {
+    this.setState({ popup: true })
   }
 
   render() {
@@ -70,10 +114,9 @@ class LandingPage extends Component {
           {this.state.data !== null &&
             <div>
               <div> {this.state.data.name} </div>
-              <div> </div>
-              <div> {this.state.data.address}</div>
-              <div> </div>
-              <div> {this.state.data.description} </div>
+              <div> Address: {this.state.data.address}</div>
+              <div> Description: {this.state.data.description} </div>
+              <div> Distance: {this.state.distance.toFixed(2)} KM </div>
             </div>
           }
         </Rodal>
@@ -86,17 +129,41 @@ class LandingPage extends Component {
             width: "100vw"
           }}
           >
-          <Layer type="circle" id="marker" radius={400} fillColor='#f87362'>
+            <Layer type="circle" id="marker" radius={400} fillColor='#f87362'>
+              {
+                this.props.data.homes.map(k => (
+                    <Feature
+                      coordinates={[k.longitude, k.latitude]}
+                      onMouseEnter={() => <Tooltip/>}
+                      key={k}
+                    />
+
+                ))
+              }
+            </Layer>
             {
               this.props.data.homes.map(k => (
-                <Feature
-                  onClick={() => this.show(k)}
-                  coordinates={[k.longitude, k.latitude]}
-                  key={k}
-                />
+                <div>
+                    <Popup
+                      className='popup-marker-v1'
+                      coordinates={[k.longitude, k.latitude]}
+                      offset={{
+                        'bottom-left': [12, -38],  'bottom': [0, -10], 'bottom-right': [-12, -38]
+                      }}>
+
+                        <div className='popup-marker-v1'>{k.type} / ${k.price}</div>
+
+                    </Popup>
+                  <Marker
+                    coordinates={[k.longitude, k.latitude]}
+                    onClick={() => this.haversine([parseFloat(k.longitude), parseFloat(k.latitude)], k)}
+                    anchor="bottom">
+                    <img src={'http://maps.google.com/mapfiles/ms/icons/red-dot.png'}/>
+                  </Marker>
+                </div>
               ))
             }
-          </Layer>
+
           <ZoomControl/>
         </Map>
       </div>
@@ -107,11 +174,13 @@ class LandingPage extends Component {
 const query = gql`
   {
     homes {
+      type,
       name,
       address,
       latitude,
       longitude,
       description,
+      price,
     }
   }
 `;
